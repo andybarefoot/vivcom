9645f15f9ae2b7e4711a5c0e9afaeefe13cde65c
Andy Barefoot <andy@andybarefoot.com>
2016-09-06 11:08:31 +0100

    home page

--- index.php
+++ index.php
@@ -92,12 +92,12 @@
                 </p>
                <h2>Our Social Links:</h2>
                 <p>
-                    <a href="https://www.facebook.com/Vision-For-Vulnerable-Communities-Foundation-VIVCOM-Foundation-861875257238221" target="facebook"><img class="social-icon" src="../images/icons/png32/facebook.png" /></a>
-                    <a href="https://twitter.com/vivcomfoundat1" target="twitter"><img class="social-icon" src="../images/icons/png32/twitter.png" /></a>
-                    <a href="https://www.youtube.com/channel/UCZvovuMv0XCa9HqysxEtEAw" target="youtube"><img class="social-icon" src="../images/icons/png32/youtube.png" /></a>
-                    <a href="https://www.instagram.com/vivcom_foundation/" target="instagram"><img class="social-icon" src="../images/icons/png32/instagram.png" /></a>
+                    <a href="https://www.facebook.com/Vision-For-Vulnerable-Communities-Foundation-VIVCOM-Foundation-861875257238221" target="facebook"><img class="social-icon" src="images/icons/png32/facebook.png" /></a>
+                    <a href="https://twitter.com/vivcomfoundat1" target="twitter"><img class="social-icon" src="images/icons/png32/twitter.png" /></a>
+                    <a href="https://www.youtube.com/channel/UCZvovuMv0XCa9HqysxEtEAw" target="youtube"><img class="social-icon" src="images/icons/png32/youtube.png" /></a>
+                    <a href="https://www.instagram.com/vivcom_foundation/" target="instagram"><img class="social-icon" src="images/icons/png32/instagram.png" /></a>
 <!--                    <a href="???" target="google"><img class="social-icon" src="images/icons/png32/google.png" /></a>       -->
-                    <a href="mailto:vivcomfoundation@gmail.com"><img class="social-icon" src="../images/icons/png32/email.png" /></a>
+                    <a href="mailto:vivcomfoundation@gmail.com"><img class="social-icon" src="images/icons/png32/email.png" /></a>
                 </p>
                 <h2>Address:</h2>
                 <p><strong>Vision For Vulnerable Communities Foundation</strong><br/>
