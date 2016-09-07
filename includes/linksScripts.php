0f5646a778b68359670f6bf60eba4a6803d9537d
Andy Barefoot <andy@andybarefoot.com>
2016-09-07 22:21:50 +0100

    hover nav

--- includes/linksScripts.php
+++ includes/linksScripts.php
@@ -3,37 +3,95 @@
         <link rel="stylesheet" type="text/css" href="css/reset.css">
         <link rel="stylesheet" type="text/css" href="css/main.css">
         <script>
-            jQuery(document).ready(function( $ ) {
-                $(window).resize(function() {
-                    if($( window ).width()>=900){
-                        $("#menu").show();
+            function positionMenus(){
+                if($( window ).width()>=900){
+                    var aboutOffset = {
+                        left: $(".about-link").offset().left -67,
+                        top: 70
                     }
-                });
-                $(function(){
-                    $("#menu-icon").click(function(){
-                        $("#menu").slideToggle();
-                    });
-                });
-                $(function(){
+                    $("#aboutMenu").offset(aboutOffset);                        
+
+                    var progOffset = {
+                        left: $(".program-link").offset().left -157,
+                        top: 70
+                    }
+                    $("#programMenu").offset(progOffset);                        
+                    var helpOffset = {
+                        left: $(".help-link").offset().left -17,
+                        top: 70
+                    }
+                    $("#helpMenu").offset(helpOffset);                        
+                }
+            }
+
+            function addTriggers(){
+                if($( window ).width()<900){
+                    $(".about-link").unbind('mouseover');
+                    $(".program-link").unbind('mouseover');
+                    $(".help-link").unbind('mouseover');
+                    $(".about-link").unbind('mouseout');
+                    $(".program-link").unbind('mouseout');
+                    $(".help-link").unbind('mouseout');
                     $(".about-link").click(function(){
                         $("#programMenu").hide();
                         $("#helpMenu").hide();
-                        $("#aboutMenu").slideToggle();
+                        $("#aboutMenu").toggle();
+                        positionMenus();
                     });
-                });
-                $(function(){
                     $(".program-link").click(function(){
                         $("#aboutMenu").hide();
                         $("#helpMenu").hide();
-                        $("#programMenu").slideToggle();
+                        $("#programMenu").toggle();
+                        positionMenus();
                     });
-                });
-                $(function(){
                     $(".help-link").click(function(){
                         $("#programMenu").hide();
                         $("#aboutMenu").hide();
-                        $("#helpMenu").slideToggle();
+                        $("#helpMenu").toggle();
+                        positionMenus();
                     });
+                }else{
+                    $(".about-link").unbind('click');
+                    $(".program-link").unbind('click');
+                    $(".help-link").unbind('click');
+                    $(".about-link").mouseover(function(){
+                        $("#aboutMenu").show();
+                        positionMenus();
+                    });
+                    $(".about-link").mouseout(function(){
+                        $("#aboutMenu").hide();
+                        positionMenus();
+                    });
+                    $(".program-link").mouseover(function(){
+                        $("#programMenu").show();
+                        positionMenus();
+                    });
+                    $(".program-link").mouseout(function(){
+                        $("#programMenu").hide();
+                        positionMenus();
+                    });
+                    $(".help-link").mouseover(function(){
+                        $("#helpMenu").show();
+                        positionMenus();
+                    });
+                    $(".help-link").mouseout(function(){
+                        $("#helpMenu").hide();
+                        positionMenus();
+                    });
+                }
+            }
+
+            jQuery(document).ready(function( $ ) {
+                addTriggers();
+                $("#menu-icon").click(function(){
+                    $("#menu").slideToggle();
+                });
+                $(window).resize(function() {
+                    addTriggers();
+                    positionMenus();
+                    if($( window ).width()>=900){
+                        $("#menu").show();
+                    }
                 });
             });
         </script>
