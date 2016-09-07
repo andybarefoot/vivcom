da8b541ca86b44413b02dc9e08a9c28202b2e8ad
Andy Barefoot <andy@andybarefoot.com>
2016-09-07 22:24:23 +0100

    hover nav

--- includes/navigation1.php
+++ includes/navigation1.php
@@ -7,36 +7,39 @@
                 <ul id="menu">
                     <li><a href="../">Home</a></li>
                     <li><a href="#" class="about-link">About Us</a></li>
-                    <li class="sub-menu"><ul id="aboutMenu">
-                        <li><a href="../team/about-us.php">Who we are</a></li>
-                        <li><a href="../team/syrill-kizza.php">Syrill Kizza</a></li>
-                        <li><a href="../team/brenda-econia.php">Brenda Econia</a></li>
-                        <li><a href="../team/abraham-olupot.php">Abraham Olupot</a></li>
-                        <li><a href="../team/kisinde-arthur.php">Kisinde Rogers Arthur</a></li>
-                        <li><a href="../team/lukanga-godfrey.php">Lukanga Musoke Godfrey</a></li>
-                    </ul></li>
-                    <li><a href="#" class="program-link">Programs</a></li>
-                    <li class="sub-menu"><ul id="programMenu">
-                        <li><a href="../programs.php">Program Overview</a></li>
-                        <li><a href="../programs/ets.php">Education and Training Support (ETS) Program</a></li>
-                        <li><a href="../programs/ccs.php">Counseling and Care Support (CCS) Program</a></li>
-                        <li><a href="../programs/vip.php">Volunteer and Internship Placement (V.I.P) Program</a></li>
-                        <li><a href="../programs/cep.php">Cultural Exchange Program</a></li>
-                        <li><a href="../programs/health-care.php">Health Care</a></li>
-                        <li><a href="../programs/agricultural.php">Agricultural Project/Granary/Food Basket</a></li>
-                        <li><a href="../programs/social.php">Social & Spiritual Development</a></li>
-                        <li><a href="../programs/sports.php">Sports and Leisure</a></li>
-                        <li><a href="../programs/efr.php">Economic and Fundraisng (EFR) Program</a></li>
-                        <li><a href="../programs/business.php">Business Development</a></li>
-                    </ul></li>
-                    <li><a href="#" class="help-link">How To Help</a></li>
-                    <li class="sub-menu"><ul id="helpMenu">
-                        <li><a href="../sponsor-a-child.php">Sponsor a Child</a></li>
-                        <li><a href="../fundraising-volunteer.php">Volunteer</a></li>
-                        <li><a href="../donate.php">Donate</a></li>
-                        <li><a href="../slum-tour.php">Slum Tour</a></li>
-                        <li><a href="../how-you-can-help.php">Other ideas</a></li>
-                    </ul></li>
+                   <li class="about-link"><a href="#">About Us</a><ul id="aboutMenu" class="sub-menu">
+                            <li><a href="../team/about-us.php">Who we are</a></li>
+                            <li><a href="../team/syrill-kizza.php">Syrill Kizza</a></li>
+                            <li><a href="../team/brenda-econia.php">Brenda Econia</a></li>
+                            <li><a href="../team/abraham-olupot.php">Abraham Olupot</a></li>
+                            <li><a href="../team/kisinde-arthur.php">Kisinde Rogers Arthur</a></li>
+                            <li><a href="../team/lukanga-godfrey.php">Lukanga Musoke Godfrey</a></li>
+                        </ul></li>
+                    <li class="program-link"><a href="#">Programs</a>
+                        <ul id="programMenu" class="sub-menu">
+                            <li><a href="../programs.php">Program Overview</a></li>
+                            <li><a href="../programs/ets.php">Education and Training Support (ETS) Program</a></li>
+                            <li><a href="../programs/ccs.php">Counseling and Care Support (CCS) Program</a></li>
+                            <li><a href="../programs/vip.php">Volunteer and Internship Placement (V.I.P) Program</a></li>
+                            <li><a href="../programs/cep.php">Cultural Exchange Program</a></li>
+                            <li><a href="../programs/health-care.php">Health Care</a></li>
+                            <li><a href="../programs/agricultural.php">Agricultural Project/Granary/Food Basket</a></li>
+                            <li><a href="../programs/social.php">Social & Spiritual Development</a></li>
+                            <li><a href="../programs/sports.php">Sports and Leisure</a></li>
+                            <li><a href="../programs/efr.php">Economic and Fundraisng (EFR) Program</a></li>
+                            <li><a href="../programs/business.php">Business Development</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="help-link"><a href="#">How To Help</a>
+                        <ul id="helpMenu" class="sub-menu">
+                            <li><a href="../sponsor-a-child.php">Sponsor a Child</a></li>
+                            <li><a href="../fundraising-volunteer.php">Volunteer</a></li>
+                            <li><a href="../donate.php">Donate</a></li>
+                            <li><a href="../slum-tour.php">Slum Tour</a></li>
+                            <li><a href="../how-you-can-help.php">Other ideas</a></li>
+                        </ul>
+                    </li>
                     <li><a href="../donate.php" class="button">Donate</a></li>
                     <li><a href="../contact-us.php">Contact Us</a></li>
                 </ul>
